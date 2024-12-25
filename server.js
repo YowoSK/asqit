24f917798c0b7393a55f357918cdef9e153b4b4c
Yowo <83427860+YowoSK@users.noreply.github.com>
2024-12-25 04:02:54 +0100

    added multiplayer feature

--- server.js
+++ server.js
@@ -1,28 +1,47 @@
 const express = require("express");
 const fs = require("fs");
+const session = require('express-session');
+const crypto = require('crypto');
 const app = express();
 const port = 3000;
 
+const secret = crypto.randomBytes(64).toString('hex');
+
 let questions = {};
 let originalQuestions = {};
-let askedQuestions = {};
-let exhaustedCategories = {};
 
 fs.readFile("questions.json", (err, data) => {
   if (err) throw err;
   questions = JSON.parse(data);
   originalQuestions = JSON.parse(JSON.stringify(questions)); // Make a copy of the original questions
-  for (let category in questions) {
-    askedQuestions[category] = [];
-    exhaustedCategories[category] = false;
-  }
 });
 
 app.use(express.static("public"));
 
+app.use(session({
+    secret: secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, maxAge: 24 * 60 * 60 * 1000 } //1 den cookie <3
+}));
+
+app.get('/start', (req, res) => {
+    if (!req.session.userId) {
+        req.session.userId = crypto.randomBytes(16).toString('hex'); // da unique ID kazdej session
+        req.session.questions = JSON.parse(JSON.stringify(originalQuestions));
+        req.session.askedQuestions = {};
+        req.session.exhaustedCategories = {};
+        for (let category in req.session.questions) {
+            req.session.askedQuestions[category] = [];
+            req.session.exhaustedCategories[category] = false;
+        }
+    }
+    res.send({ userId: req.session.userId });
+});
+
 app.get("/categories", (req, res, next) => {
   try {
-    res.json(Object.keys(questions));
+    res.json(Object.keys(req.session.questions));
   } catch (err) {
     next(err);
   }
@@ -30,7 +49,7 @@ app.get("/categories", (req, res, next) => {
 
 app.get("/category-status", (req, res, next) => {
   try {
-    res.json(exhaustedCategories);
+    res.json(req.session.exhaustedCategories);
   } catch (err) {
     next(err);
   }
@@ -39,15 +58,15 @@ app.get("/category-status", (req, res, next) => {
 app.get("/question", (req, res, next) => {
   try {
     const category = req.query.category;
-    if (!category || !questions[category]) {
+    if (!category || !req.session.questions[category]) {
       const err = new Error("Invalid category");
       err.status = 400;
       throw err;
     }
 
-    if (questions[category].length === 0) {
-      exhaustedCategories[category] = true;
-      const message = Object.values(exhaustedCategories).every(
+    if (req.session.questions[category].length === 0) {
+      req.session.exhaustedCategories[category] = true;
+      const message = Object.values(req.session.exhaustedCategories).every(
         (status) => status
       )
         ? "All categories are exhausted. No more questions available."
@@ -56,10 +75,10 @@ app.get("/question", (req, res, next) => {
     }
 
     let randomQuestionIndex = Math.floor(
-      Math.random() * questions[category].length
+      Math.random() * req.session.questions[category].length
     );
-    let question = questions[category].splice(randomQuestionIndex, 1)[0];
-    askedQuestions[category].push(question);
+    let question = req.session.questions[category].splice(randomQuestionIndex, 1)[0];
+    req.session.askedQuestions[category].push(question);
 
     res.json(question);
   } catch (err) {
@@ -69,10 +88,12 @@ app.get("/question", (req, res, next) => {
 
 app.post("/reset", (req, res, next) => {
   try {
-    questions = JSON.parse(JSON.stringify(originalQuestions));
-    for (let category in questions) {
-      askedQuestions[category] = [];
-      exhaustedCategories[category] = false;
+    req.session.questions = JSON.parse(JSON.stringify(originalQuestions));
+    req.session.askedQuestions = {};
+    req.session.exhaustedCategories = {};
+    for (let category in req.session.questions) {
+      req.session.askedQuestions[category] = [];
+      req.session.exhaustedCategories[category] = false;
     }
     res.json({ message: "Questions have been reset." });
   } catch (err) {
